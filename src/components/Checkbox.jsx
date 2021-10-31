c0c18a2b86294a94d9fd60150dda6edb2ba3e2af
thamika-git <adheesha@raditha.com>
2021-10-31 10:48:40 +0530

    Fix some small issues in the Checkbox component.

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,13 +1,15 @@
 function Checkbox(props) {
     return (
         <div className="mt-4">
-            <label class="inline-flex items-center">
+            <label className="inline-flex items-center">
                 <input
                     type="checkbox"
                     className="rounded bg-gray-200 border-none"
-                    checked={props.checked}
+                    defaultChecked={props.checked}
                 />
-                <span class="ml-2 text-lg text-gray-700">{props.label}</span>
+                <span className="ml-2 text-lg text-gray-700">
+                    {props.label}
+                </span>
             </label>
         </div>
     );
