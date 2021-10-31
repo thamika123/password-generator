aa16c102add76596c3e4c53301c11386a425b96d
thamika-git <adheesha@raditha.com>
2021-10-31 11:24:16 +0530

    Add a card to show generated password.

--- src/components/Length.jsx
+++ src/components/Length.jsx
@@ -1,14 +1,14 @@
 function Length(props) {
     return (
         <div className="mb-10 text-gray-700">
-            <label className="mr-8 block">
+            <label className="block">
                 <span className="text-lg">{props.label}</span>
                 <input
                     type="number"
                     min={props.min}
                     max={props.max}
                     defaultValue={props.default}
-                    className="rounded-md mr-8 w-full bg-gray-100 border-none mt-2"
+                    className="rounded-md w-full bg-gray-100 border-none mt-2"
                 />
             </label>
         </div>
--- src/index.jsx
+++ src/index.jsx
@@ -4,31 +4,42 @@ import ReactDOM from "react-dom";
 import Checkbox from "./components/Checkbox";
 import Length from "./components/Length";
 
+function Form() {
+    return (
+        <div className="bg-gray-50 md:w-144 min-w-80 rounded-2xl shadow-lg px-8 py-8">
+            <Length label="Password length:" min="6" max="20" default="15" />
+            <Checkbox label="Include uppercase characters" checked={true} />
+            <Checkbox label="Include lowercase characters" checked={true} />
+            <Checkbox label="Include numbers" />
+            <Checkbox label="Include symbols" />
+        </div>
+    );
+}
+
+function Password(props) {
+    return (
+        <div className="bg-gray-50 md:w-144 min-w-80 rounded-2xl shadow-lg px-8 py-8 mb-12">
+            <h2 className="text-lg text-gray-700">Generated password:</h2>
+            <div className="width-full bg-gray-100 px-4 py-2 rounded-lg mt-2">
+                <h2 className="text-gray-500">
+                    {props.password
+                        ? props.password
+                        : "Choose your options and click generate password."}
+                </h2>
+            </div>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div>
-            <h1 className="mt-56 text-4xl font-sans font-extrabold text-gray-700 text-center">
+            <h1 className="md:mt-56 mt-40 text-4xl font-sans font-extrabold text-gray-700 text-center">
                 Password Generator
             </h1>
-            <div className="flex items-center justify-center mt-16 mb-12">
-                <div className="bg-gray-50 w-144 min-w-80 rounded-2xl shadow-lg pl-8 py-8">
-                    <Length
-                        label="Password length:"
-                        min="6"
-                        max="20"
-                        default="15"
-                    />
-                    <Checkbox
-                        label="Include uppercase characters"
-                        checked={true}
-                    />
-                    <Checkbox
-                        label="Include lowercase characters"
-                        checked={true}
-                    />
-                    <Checkbox label="Include numbers" />
-                    <Checkbox label="Include symbols" />
-                </div>
+            <div className="flex flex-col items-center justify-center mt-16 mb-12">
+                <Password />
+                <Form />
             </div>
         </div>
     );
