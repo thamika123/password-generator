285ab4ed92c402a828f64d5c8b44bdea5b6e72c6
thamika-git <adheesha@raditha.com>
2021-10-31 14:13:31 +0530

    Move length and allowed characters into state.

--- src/index.jsx
+++ src/index.jsx
@@ -5,25 +5,33 @@ import Password from "./components/Password";
 import Form from "./components/Form";
 import Button from "./components/Button";
 
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const upper = lower.toUpperCase();
+const numbers = "0123456789";
+const symbols = "!@#$%^&*();/.,\\`<>[]{}'\"?";
+
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { password: undefined };
 
-        this.handleButtonClick = this.handleButtonClick.bind(this);
+        this.state = {
+            password: undefined,
+            length: 15,
+            allowedChars: `${lower}${upper}`,
+        };
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleButtonClick() {
-        let allowedChars = "abcdefghijklmnopqrstuvwxyz";
-        allowedChars += allowedChars.toUpperCase();
-
-        this.setState({ password: this.generatePassword(15, allowedChars) });
+    handleClick() {
+        this.setState({ password: this.generatePassword() });
     }
 
-    generatePassword(length, allowedChars) {
+    generatePassword() {
         // This code is based on https://stackoverflow.com/a/51540480/16027708
-        let array = new Uint32Array(length);
+        let array = new Uint32Array(this.state.length);
         let randomNumbers = Array.from(crypto.getRandomValues(array));
+        let allowedChars = this.state.allowedChars;
 
         return randomNumbers
             .map(x => allowedChars[x % allowedChars.length])
@@ -41,7 +49,7 @@ class App extends React.Component {
                     <Form />
                     <Button
                         text="Generate password"
-                        onClick={this.handleButtonClick}
+                        onClick={this.handleClick}
                     />
                 </div>
             </div>
